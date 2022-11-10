6b7163527ab90e9a2a71165c43a6ad8a95a1db4f
Yuchi <yuchi518@gmail.com>
2022-11-10 15:09:47 +0800

    First version to load gin from dij automatically.

--- executor/webserver.go
+++ executor/webserver.go
@@ -3,6 +3,7 @@ package executor
 import (
 	"context"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/letscool/lc-go/dij"
 	. "github.com/letscool/lc-go/lg"
 	"golang.org/x/net/netutil"
@@ -18,9 +19,106 @@ import (
 
 const (
 	DefaultWebServerPort = 8000
-	TagName              = "http"
+	HttpTagName          = "http"
 	WebConfigKey         = "webserver.config"
 )
+
+type WebContext struct {
+	*gin.Context
+}
+
+type HandlerFunc func(*WebContext)
+
+type WebServerSpec interface {
+	WebControllerSpec
+	iAmAWebServer()
+}
+
+type WebServer struct {
+	WebController
+}
+
+func (w *WebServer) iAmAWebServer() {
+
+}
+
+type WebControllerSpec interface {
+	iAmAWebController()
+	SetupRouter(router gin.IRouter)
+}
+
+type WebController struct {
+}
+
+func (w *WebController) iAmAWebController() {
+
+}
+
+func (w *WebController) SetupRouter(router gin.IRouter) {
+	fmt.Printf("Should setup router?\n")
+}
+
+type WebMiddlewareSpec interface {
+	iAmAWebMiddleware()
+	GetHandlers() []HandlerFunc
+}
+
+type WebMiddleware struct {
+}
+
+func (m *WebMiddleware) iAmAWebMiddleware() {
+
+}
+
+func (m *WebMiddleware) GetHandlers() []HandlerFunc {
+	fmt.Printf("Should provide handlers?\n")
+	return nil
+}
+
+//func IsTypeOfWebServer(typ reflect.Type) bool {
+//	wssTyp := reflect.TypeOf((*WebServerSpec)(nil)).Elem()
+//	fmt.Printf("wssTyp: %v\n", wssTyp)
+//	return typ.Implements(wssTyp)
+//}
+
+func IsTypeOfWebServer(typ reflect.Type) bool {
+	if typ.Kind() == reflect.Pointer {
+		if elemTyp := typ.Elem(); elemTyp.Kind() == reflect.Struct {
+			return IsTypeOfWebServer(elemTyp)
+		}
+		return false
+	}
+	instPtrValue := reflect.New(typ)
+	instIf := instPtrValue.Interface()
+	_, ok := instIf.(WebServerSpec)
+	return ok
+}
+
+func IsTypeOfWebController(typ reflect.Type) bool {
+	if typ.Kind() == reflect.Pointer {
+		if elemTyp := typ.Elem(); elemTyp.Kind() == reflect.Struct {
+			return IsTypeOfWebController(elemTyp)
+		}
+		return false
+	}
+	instPtrValue := reflect.New(typ)
+	instIf := instPtrValue.Interface()
+	_, ok := instIf.(WebControllerSpec)
+	return ok
+}
+
+func IsTypeOfWebMiddleware(typ reflect.Type) bool {
+	if typ.Kind() == reflect.Pointer {
+		if elemTyp := typ.Elem(); elemTyp.Kind() == reflect.Struct {
+			return IsTypeOfWebMiddleware(elemTyp)
+		}
+		return false
+	}
+	instPtrValue := reflect.New(typ)
+	instIf := instPtrValue.Interface()
+	_, ok := instIf.(WebMiddlewareSpec)
+	return ok
+}
 
 type WebServerConfig struct {
 	address string
@@ -40,7 +138,7 @@ func CreateServeMux(servInstPtr any) *http.ServeMux {
 
 	for j := 0; j < servType.NumField(); j++ {
 		fieldSpec := servType.Field(j)
-		_, existingHttpTag := fieldSpec.Tag.Lookup(TagName)
+		_, existingHttpTag := fieldSpec.Tag.Lookup(HttpTagName)
 		if existingHttpTag {
 			// f := func(writer http.ResponseWriter, r *http.Request) {}
 		}
@@ -50,7 +148,7 @@ func CreateServeMux(servInstPtr any) *http.ServeMux {
 }
 
 func LaunchWebServer(webServerType reflect.Type, others ...any) {
-	ref := map[dij.DependencyKey]any{}
+	ref := dij.DependencyReference{}
 	config := WebServerConfig{}
 	for _, other := range others {
 		otherTyp := reflect.TypeOf(other)
@@ -112,4 +210,157 @@ func LaunchWebServer(webServerType reflect.Type, others ...any) {
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Printf("graceful shutdown failed: %v\n", err)
 	}
+}
+
+func PrepareGin(webServerType reflect.Type, others ...any) (*gin.Engine, dij.DependencyReferencePtr, error) {
+	ref := dij.DependencyReference{}
+	config := WebServerConfig{}
+	for _, other := range others {
+		otherTyp := reflect.TypeOf(other)
+		switch v := other.(type) {
+		case WebServerConfig:
+			config = v
+		default:
+			log.Println("No ideal about type:", otherTyp)
+		}
+	}
+	ref[WebConfigKey] = config
+
+	if !IsTypeOfWebServer(webServerType) {
+		return nil, nil, fmt.Errorf("the type(%v) is not a web server", webServerType)
+	}
+
+	instPtr, err := dij.CreateInstance(webServerType, &ref, "^")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	router := gin.Default()
+
+	if err := setupRouter(instPtr, webServerType, router, &ref); err != nil {
+		return nil, nil, err
+	}
+
+	return router, &ref, nil
+}
+
+func LaunchGin(webServerType reflect.Type, others ...any) error {
+	engine, refPtr, err := PrepareGin(webServerType, others)
+	if err != nil {
+		return err
+	}
+	v, _ := refPtr.Get(WebConfigKey)
+	config := v.(WebServerConfig)
+
+	addr := fmt.Sprintf("%v:%d", config.address, Ife(config.port <= 0, DefaultWebServerPort, config.port))
+	return engine.Run(addr)
+}
+
+func setupRouter(instPtr any, instType reflect.Type, router gin.IRouter, refPtr dij.DependencyReferencePtr) error {
+	predecessor := make([]int, 0)
+	plugins := make([]int, 0)
+	extenders := make([]int, 0)
+	for i := 0; i < instType.NumField(); i++ {
+		field := instType.Field(i)
+		fieldTyp := field.Type
+		if IsTypeOfWebController(fieldTyp) {
+			if field.Anonymous {
+				predecessor = append(predecessor, i)
+			} else {
+				extenders = append(extenders, i)
+			}
+		} else if IsTypeOfWebMiddleware(fieldTyp) {
+			plugins = append(plugins, i)
+		}
+	}
+
+	// setup current router
+	if len(predecessor) != 1 {
+		return fmt.Errorf("struct '%s' should embeded web controller or web server.(%d)", instType.Name(), len(predecessor))
+	} else {
+		field := instType.Field(predecessor[0])
+		tag, exists := field.Tag.Lookup(HttpTagName)
+		if exists {
+			attrs := ParseStructTag(tag)
+			nameAttr, existingName := attrs.FirstAttrWithValOnly()
+			if existingName {
+				router = router.Group(nameAttr.Val)
+			}
+		}
+		ctrl := instPtr.(WebControllerSpec)
+		ctrl.SetupRouter(router)
+	}
+
+	// setup middlewares
+	if len(plugins) > 0 {
+		for _, idx := range plugins {
+			field := instType.Field(idx)
+			fieldTyp := field.Type
+			fmt.Printf("Middleware: %v from %v\n", fieldTyp, instType)
+			if fieldTyp.Kind() != reflect.Pointer || fieldTyp.Elem().Kind() != reflect.Struct {
+				return fmt.Errorf("middleware's type(%v) should be a kind of struct point", fieldTyp)
+			}
+			instValue := reflect.ValueOf(instPtr).Elem()
+			var fieldIf any
+			var exists bool
+			fieldIf, exists = refPtr.GetForDiField(instType, idx)
+			if !exists {
+				fieldValue := instValue.Field(idx)
+				if fieldValue.IsZero() {
+					return fmt.Errorf("field(%s) should not be zero", fieldTyp.Name())
+				}
+				if field.IsExported() {
+					fieldIf = fieldValue.Interface()
+				} else {
+					fieldIf = reflect.NewAt(fieldTyp, fieldValue.Addr().UnsafePointer()).Elem().Interface()
+				}
+			} else {
+				//fmt.Printf("middleware load from dij: %v\n", fieldTyp)
+			}
+			handlers := fieldIf.(WebMiddlewareSpec).GetHandlers()
+			if len(handlers) > 0 {
+				for _, handler := range handlers {
+					router.Use(func(c *gin.Context) {
+						// TODO: refine this
+						handler(&WebContext{c})
+					})
+				}
+			}
+		}
+	}
+
+	// setup extenders
+	if len(extenders) > 0 {
+		for _, idx := range extenders {
+			field := instType.Field(idx)
+			fieldTyp := field.Type
+			fmt.Printf("Extender: %v from %v\n", fieldTyp, instType)
+			if fieldTyp.Kind() != reflect.Pointer || fieldTyp.Elem().Kind() != reflect.Struct {
+				return fmt.Errorf("appending controller's type(%v) should be a kind of struct point", fieldTyp)
+			}
+			instValue := reflect.ValueOf(instPtr).Elem()
+			var fieldIf any
+			var exists bool
+			fieldIf, exists = refPtr.GetForDiField(instType, idx)
+			if !exists {
+				fieldValue := instValue.Field(idx)
+				if fieldValue.IsZero() {
+					return fmt.Errorf("field(%s) should not be zero", fieldTyp.Name())
+				}
+				if field.IsExported() {
+					fieldIf = fieldValue.Interface()
+				} else {
+					fieldIf = reflect.NewAt(fieldTyp, fieldValue.Addr().UnsafePointer()).Elem().Interface()
+				}
+			} else {
+				//fmt.Printf("extenders load from dij: %v\n", fieldTyp)
+			}
+			if err := setupRouter(fieldIf, fieldTyp.Elem(), router, refPtr); err != nil {
+				return err
+			}
+		}
+	}
+
+	// all done
+	return nil
 }
--- executor/webserver_test.go
+++ executor/webserver_test.go
@@ -1,12 +1,137 @@
 package executor
 
 import (
-	"github.com/letscool/lc-go/dij"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"reflect"
 	"testing"
 )
 
-// go test ./executor -v -run TestWeb
-func TestWeb(t *testing.T) {
-	dij.EnableLog()
+type TestByWebServerValue struct {
+	WebServer
+}
 
+type TestByWebServerPtr struct {
+	*WebServer
+}
+
+type TestByWebServerValueExt struct {
+	WebServer
+}
+
+func (w TestByWebServerValueExt) iAmAWebServer() {
+
+}
+
+type TestByWebMiddlewareValue struct {
+	WebMiddleware
+}
+
+// go test ./executor -v -run TestValidateWebType
+func TestValidateWebType(t *testing.T) {
+	//dij.EnableLog()
+	t.Run("TestByWebServerValue", func(t *testing.T) {
+		ws := TestByWebServerValue{}
+		if typ := reflect.TypeOf(ws); !IsTypeOfWebServer(typ) {
+			t.Errorf("%v is not a web server type\n", typ)
+			if !IsTypeOfWebController(typ) {
+				t.Errorf("%v is not a web controller type\n", typ)
+			}
+		}
+		if typ := reflect.TypeOf(&ws); !IsTypeOfWebServer(typ) {
+			t.Errorf("%v is not a web server type\n", typ)
+			if !IsTypeOfWebController(typ) {
+				t.Errorf("%v is not a web controller type\n", typ)
+			}
+		}
+	})
+
+	t.Run("TestByWebServerPtr", func(t *testing.T) {
+		ws := TestByWebServerPtr{}
+		if typ := reflect.TypeOf(ws); !IsTypeOfWebServer(typ) {
+			t.Errorf("%v is not a web server type\n", typ)
+			if !IsTypeOfWebController(typ) {
+				t.Errorf("%v is not a web controller type\n", typ)
+			}
+		}
+		if typ := reflect.TypeOf(&ws); !IsTypeOfWebServer(typ) {
+			t.Errorf("%v is not a web server type\n", typ)
+			if !IsTypeOfWebController(typ) {
+				t.Errorf("%v is not a web controller type\n", typ)
+			}
+		}
+	})
+
+	t.Run("TestByWebServerValueExt", func(t *testing.T) {
+		ws := TestByWebServerValueExt{}
+		if typ := reflect.TypeOf(ws); !IsTypeOfWebServer(typ) {
+			t.Errorf("%v is not a web server type\n", typ)
+			if !IsTypeOfWebController(typ) {
+				t.Errorf("%v is not a web controller type\n", typ)
+			}
+		}
+		if typ := reflect.TypeOf(&ws); !IsTypeOfWebServer(typ) {
+			t.Errorf("%v is not a web server type\n", typ)
+			if !IsTypeOfWebController(typ) {
+				t.Errorf("%v is not a web controller type\n", typ)
+			}
+		}
+	})
+
+	t.Run("TestByWebMiddlewareValue", func(t *testing.T) {
+		ws := TestByWebMiddlewareValue{}
+		if typ := reflect.TypeOf(ws); !IsTypeOfWebMiddleware(typ) {
+			t.Errorf("%v is not a web middleware type\n", typ)
+		}
+		if typ := reflect.TypeOf(&ws); !IsTypeOfWebMiddleware(typ) {
+			t.Errorf("%v is not a web middleware type\n", typ)
+		}
+	})
+}
+
+type TestWebServer struct {
+	WebServer `http:"" description:""`
+
+	ctrl1      *TestWebController1 `di:"^"`
+	Middleware *TestWebMiddleware  `di:"^"`
+}
+
+func (s *TestWebServer) SetupRouter(router gin.IRouter) {
+	router.GET("", s.GetRoot)
+}
+
+func (s *TestWebServer) GetRoot(ctx *gin.Context) {
+	ctx.IndentedJSON(http.StatusOK, "hello")
+}
+
+type TestWebController1 struct {
+	WebController `http:"path=user"`
+
+	Ctrl2 *TestWebController2 `di:"^"`
+}
+
+func (c *TestWebController1) GetUser(p struct {
+	ctx *WebContext `http:"method=get, path=" description="取得使用者資訊"`
+}) {
+	ctx := p.ctx
+	ctx.Get("")
+}
+
+type TestWebController2 struct {
+	WebController
+}
+
+type TestWebMiddleware struct {
+	WebMiddleware
+}
+
+// go test ./executor -v -run TestWebServerExec
+func TestWebServerExec(t *testing.T) {
+	t.Run("dij", func(t *testing.T) {
+		wsTyp := reflect.TypeOf(TestWebServer{})
+		//dij.EnableLog()
+		if err := LaunchGin(wsTyp); err != nil {
+			t.Error(err)
+		}
+	})
 }
